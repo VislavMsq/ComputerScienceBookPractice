f12732f799777d13028188604478470dd900c480
Vladislav <tokenvlad@gmail.com>
2023-05-04 15:24:19 +0200

    second commit

---
+++ src/main/java/conditionalJumpsAndLoops/example/example3/Test.java
@@ -0,0 +1,14 @@
+package conditionalJumpsAndLoops.example.example3;
+
+public class Test {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1000; i < 2000; i++) {
+            if(count % 5 == 0){
+                System.out.println();
+            }
+            System.out.println(i + " ");
+            count++;
+        }
+    }
+}
