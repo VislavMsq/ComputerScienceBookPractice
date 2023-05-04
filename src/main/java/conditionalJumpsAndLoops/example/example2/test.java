4eefa19ce8f8c61c24f46d2e7a86767318f660ff
Vladislav <tokenvlad@gmail.com>
2023-05-04 14:58:58 +0200

    q

--- src/main/java/conditionalJumpsAndLoops/example/example2/test.java
+++ src/main/java/conditionalJumpsAndLoops/example/example2/test.java
@@ -1,4 +1,18 @@
 package conditionalJumpsAndLoops.example.example2;
 
+import java.util.Scanner;
+
 public class test {
+    public static void main(String[] args) {
+        System.out.println("Enter you numbers!");
+        Scanner sc = new Scanner(System.in);
+        double q = sc.nextDouble();
+        double w = sc.nextDouble();
+
+        if(q > 0 && q < 1 && w > 0 && w < 1){
+            System.out.println("True");
+        } else{
+            System.out.println("False");
+        }
+    }
 }
